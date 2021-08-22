0161dfbdb2d73c64c472be57fbe0a927f56a3ba0
Tino-test <moraistrino@gmail.com>
2021-08-22 09:29:57 +0530

    Adding new codes

---
+++ src/org/test/Sampleeight.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Sampleeight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=22;
+int b=99;
+int c= a+b;
+System.out.print(c);
+	}
+
+}
